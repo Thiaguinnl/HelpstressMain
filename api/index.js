13cfb99a29ab000d4ca37dfe91d613b67c2bd4c0
Thiago Grandim <thiagograndim.m@gmail.com>
2025-06-24 16:01:02 -0300

    Middlewares

--- api/index.js
+++ api/index.js
@@ -43,6 +43,16 @@ function verifyToken(req, res, next) {
     });
 }
 
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); 
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS,PATCH');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 server.use(middlewares);
 
 server.use(jsonServer.bodyParser);
